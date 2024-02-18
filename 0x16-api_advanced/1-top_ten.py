c76e9d77a55c0b5d77864273f8530d01d8124e75
sitiabdulaziz11 <sitiabdulaziz462@gmail.com>
2024-02-18 17:56:07 +0300

    updated advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module that queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit
+the first 10 hot posts listed for a given subreddit.
 """
 
 import requests
